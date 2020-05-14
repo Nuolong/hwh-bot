e649ed230177659950fa501a999c0b2f10bbf17a
Nuolong <nmcshea@nd.edu>
2020-05-14 17:43:18 -0400

    bot added separate from client

--- bot.py
+++ bot.py
@@ -2,50 +2,35 @@
 
 import os
 import random
-
-import discord
 from dotenv import load_dotenv
+
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='`')
 
-@client.event
+@bot.event
 async def on_ready():
-	guild = discord.utils.get(client.guilds, name=GUILD)	
-
-	print(f'{client.user} is connected to:\n'
-		f'{guild.name}(id: {guild.id})\n')
-
-	members = '\n - '.join([member.name for member in guild.members])
-	print(f'Server Members:\n - {members}')
-
-@client.event
-async def on_member_join(member):
-	await member.create_dm()
-	await member.dm_channel.send(f'Heblo {member.name}, welcome to this bot test!!!111')	
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    test_responses = ['test a','test b','test c']
-
-    if message.content == 'test!':
-        response = random.choice(test_responses)
-        await message.channel.send(response)
+    print(f'{bot.user.name} connected to Discord.\n')
 
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+@bot.command(name='test', help='Just a test command.')
+async def test(ctx):
+	test_pile = ['heblo','byeblo','test3']
+	response = random.choice(test_pile)
+	await ctx.send(response)
+
+@bot.command(name='roll', help='Simulates rolling dice. `roll [dice] [sides]')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
 
 
-client.run(TOKEN)
+bot.run(TOKEN)
+
