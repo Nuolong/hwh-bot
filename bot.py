544c37722e198ea8e91214df26fed624018c98b6
Nuolong <nmcshea@nd.edu>
2020-05-14 13:51:53 -0400

    utils.get method

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)	
+	guild = discord.utils.get(client.guilds, name=GUILD)	
 
 	print(f'{client.user} is connected to:\n'
 		f'{guild.name}(id: {guild.id})\n')
