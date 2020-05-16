aace37a8c5963b94afc49663c7292671ec0aac4e
Nuolong <nmcshea@nd.edu>
2020-05-16 13:00:59 -0400

    some borrowed functionality added

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,12 +1,27 @@
 from discord.ext import commands
+from utils import text_to_owo
 
 class Basic(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+	# called when a command_error is invoked
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx, er):
+		print(er)
+		await ctx.send("Basic error message for incorrect command usage")
+
+	# converts text to owo when `owo is written
 	@commands.command()
-	async def ping(self, ctx):
-		await ctx.send("Test")
+	async def owo(self, ctx):
+		await ctx.send(text_to_owo(ctx.message.content))
+
+	# creates an invite link to the server. (only works in guild)
+	@commands.command()
+	@commands.guild_only()
+	async def invite(self, ctx):
+		link = await ctx.channel.create_invite(max_age=1) # max_age in days
+		await ctx.send(link)
 
 def setup(bot):
 	bot.add_cog(Basic(bot))
